26b3f8ea5660f178a9279cbabc1c05f6ba10630f
Hannah Paterka <paterkahannah@gmail.com>
2021-07-07 10:34:20 -0500

    walkthrough notes

--- src/main/java/ShowAdsServlet.java
+++ src/main/java/ShowAdsServlet.java
@@ -11,7 +11,8 @@ public class ShowAdsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
         // create a new instance of the ListAdsDao class so we can use the all() method
-        Ads adsDao = new ListAdsDao();
+//        Ads adsDao = new ListAdsDao(); // my old attempt
+        Ads adsDao = DaoFactory.getAdsDao();
         //set a new List of <ad objects> equal to our list of ads (.all() method)
         List<Ad> ads = adsDao.all();
         // Pass the data to the jsp
