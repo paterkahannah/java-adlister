3cb6e8e27da126311ccd832a1293332e74dc096c
Hannah Paterka <paterkahannah@gmail.com>
2021-07-13 15:37:50 -0500

    walkthrough

--- src/main/java/com/codeup/adlister/util/Password.java
+++ src/main/java/com/codeup/adlister/util/Password.java
@@ -3,7 +3,7 @@ package com.codeup.adlister.util;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class Password {
-    private static final int ROUNDS = 12;
+    private static final int ROUNDS = 12; // the number of times the password is mixed
 
     public static String hash(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt(ROUNDS));
